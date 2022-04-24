11f1840558932b3e575ba959b69ef20bb58ac214
Raphael Mota <rphmota@gmail.com>
2022-04-23 21:47:04 -0300

    final adjustments

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,28 +1,3 @@
-/* import { v4 as uuidV4, v4 } from "uuid";
-
-class User {
-  id?: string;
-
-  name: string;
-
-  email: string;
-
-  admin?: boolean;
-
-  created_at: Date;
-
-  updated_at?: Date;
-
-  constructor() {
-    if (!this.id) {
-      this.id = uuidV4();
-      this.admin = false;
-    }
-  }
-}
-
-export { User };
- */
 import { v4 as uuidV4 } from "uuid";
 
 class User {
